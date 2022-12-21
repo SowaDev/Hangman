ceda65385f5b36f5e6cf292812aa7d144b62e3ca
SowaDev <piotrsowa@yahoo.com>
2022-12-21 14:16:12 +0100

    style: format code with Prettier

--- app.js
+++ app.js
@@ -1,103 +1,136 @@
-import Draw from './draw.js'
-const words = ['marine', 'butterfly', 'house', 'animal', 'bridge', 'car', 'leaf', 'spider', 'bird', 'whale', 'keyboard']
-const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+import Draw from './draw.js';
+const words = [
+	'marine',
+	'butterfly',
+	'house',
+	'animal',
+	'bridge',
+	'car',
+	'leaf',
+	'spider',
+	'bird',
+	'whale',
+	'keyboard',
+];
+const alphabet = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+];
 let wordToGuess = words[Math.floor(Math.random() * words.length)].toUpperCase();
-var againButton = document.querySelector('.again')
-var winLoseButton = document.querySelector('.winLose')
-var goodGuesses = 0
-var badGuesses = 0
-var draw = new Draw(wordToGuess)
+var againButton = document.querySelector('.again');
+var winLoseButton = document.querySelector('.winLose');
+var goodGuesses = 0;
+var badGuesses = 0;
+var draw = new Draw(wordToGuess);
 
 const createButtons = () => {
-    for(let i = 0; i < alphabet.length; i++){
-        const firstRow = document.querySelector('.firstRow')
-        const secondRow = document.querySelector('.secondRow')
-        var letter = document.createElement("BUTTON")
-        letter.setAttribute('letterId', alphabet[i])
-        letter.innerText = alphabet[i]
-        letter.className='letterClass'
-        letter.addEventListener("click", function(){
-            handleClick(alphabet[i], this)
-        })
-        if(i < 13)
-            firstRow.appendChild(letter)
-        else
-            secondRow.appendChild(letter)
-    }
-    againButton.addEventListener('click', playAgain)
-}
+	for (let i = 0; i < alphabet.length; i++) {
+		const firstRow = document.querySelector('.firstRow');
+		const secondRow = document.querySelector('.secondRow');
+		var letter = document.createElement('BUTTON');
+		letter.setAttribute('letterId', alphabet[i]);
+		letter.innerText = alphabet[i];
+		letter.className = 'letterClass';
+		letter.addEventListener('click', function () {
+			handleClick(alphabet[i], this);
+		});
+		if (i < 13) firstRow.appendChild(letter);
+		else secondRow.appendChild(letter);
+	}
+	againButton.addEventListener('click', playAgain);
+};
 
 const playAgain = () => {
-    draw.clearContext();
-    againButton.style.visibility = 'hidden'
-    winLoseButton.style.visibility = 'hidden'
-    wordToGuess = words[Math.floor(Math.random() * words.length)].toUpperCase();
-    draw.wordToGuess = wordToGuess
-    draw.drawUnderscores()
-    goodGuesses = 0
-    badGuesses = 0
-    let letters = document.getElementsByClassName('letterClass')
-    for(let letter of letters){
-        letter.style.visibility = 'visible'
-    }
-}
+	draw.clearContext();
+	againButton.style.visibility = 'hidden';
+	winLoseButton.style.visibility = 'hidden';
+	wordToGuess = words[Math.floor(Math.random() * words.length)].toUpperCase();
+	draw.wordToGuess = wordToGuess;
+	draw.drawUnderscores();
+	goodGuesses = 0;
+	badGuesses = 0;
+	let letters = document.getElementsByClassName('letterClass');
+	for (let letter of letters) {
+		letter.style.visibility = 'visible';
+	}
+};
 
-draw.drawUnderscores()
-createButtons()
+draw.drawUnderscores();
+createButtons();
 
 const handleClick = (letter, button) => {
-    if(badGuesses < 7 && goodGuesses < wordToGuess.length){
-        check(letter)
-        button.style.visibility = 'hidden'
-    }
-}
+	if (badGuesses < 7 && goodGuesses < wordToGuess.length) {
+		check(letter);
+		button.style.visibility = 'hidden';
+	}
+};
 
-const findIndices = letter => {
-    let indices = []
-    let index = wordToGuess.indexOf(letter)
-    while(index != -1){
-        indices.push(index)
-        index = wordToGuess.indexOf(letter, index + 1)
-    }
-    return indices
-}
+const findIndices = (letter) => {
+	let indices = [];
+	let index = wordToGuess.indexOf(letter);
+	while (index != -1) {
+		indices.push(index);
+		index = wordToGuess.indexOf(letter, index + 1);
+	}
+	return indices;
+};
 
-const check = letter => {
-    if(wordToGuess.toUpperCase().includes(letter)){
-        handleGoodGuess(letter)
-    } else {
-        badGuesses++
-        handleBadGuess(badGuesses);
-    }
-}
+const check = (letter) => {
+	if (wordToGuess.toUpperCase().includes(letter)) {
+		handleGoodGuess(letter);
+	} else {
+		badGuesses++;
+		handleBadGuess(badGuesses);
+	}
+};
 
-const handleGoodGuess = letter => {
-    let indices = findIndices(letter)
-    draw.drawLetter(letter, indices)
-    goodGuesses += indices.length
-    if(goodGuesses === wordToGuess.length){
-        document.querySelector('.again').style.visibility = 'visible'
-        winLoseButton.innerText = "You win"
-        winLoseButton.style.visibility = 'visible'
-    }
-}
+const handleGoodGuess = (letter) => {
+	let indices = findIndices(letter);
+	draw.drawLetter(letter, indices);
+	goodGuesses += indices.length;
+	if (goodGuesses === wordToGuess.length) {
+		document.querySelector('.again').style.visibility = 'visible';
+		winLoseButton.innerText = 'You win';
+		winLoseButton.style.visibility = 'visible';
+	}
+};
 
-const handleBadGuess = badGuesses => {
-    if(badGuesses === 1)
-        draw.drawHanger();
-    else
-        draw.drawHangman(badGuesses)
-    if(badGuesses === 7)
-        handleLose()
-}
+const handleBadGuess = (badGuesses) => {
+	if (badGuesses === 1) draw.drawHanger();
+	else draw.drawHangman(badGuesses);
+	if (badGuesses === 7) handleLose();
+};
 
 const handleLose = () => {
-    document.querySelector('.again').style.visibility = 'visible'
-    for(let letter of wordToGuess){
-        let indices = findIndices(letter)
-        draw.drawLetter(letter, indices)
-    }
-    winLoseButton.innerText = "You Lose"
-    winLoseButton.style.visibility = 'visible'
-}
+	document.querySelector('.again').style.visibility = 'visible';
+	for (let letter of wordToGuess) {
+		let indices = findIndices(letter);
+		draw.drawLetter(letter, indices);
+	}
+	winLoseButton.innerText = 'You Lose';
+	winLoseButton.style.visibility = 'visible';
+};
--- draw.js
+++ draw.js
@@ -1,138 +1,133 @@
 class Draw {
-    constructor(wordToGuess){
-        this._wordToGuess = wordToGuess
-        this.unCanvas = this.createCanvas('.letters', 800, 150)
-        this._ctx = this.unCanvas.getContext('2d')
-        this.haCanvas = this.createCanvas('.hanger', 500, 500)
-        this._context = this.haCanvas.getContext('2d')
-        this.drawXCoordinates = []
-    }
+	constructor(wordToGuess) {
+		this._wordToGuess = wordToGuess;
+		this.unCanvas = this.createCanvas('.letters', 800, 150);
+		this._ctx = this.unCanvas.getContext('2d');
+		this.haCanvas = this.createCanvas('.hanger', 500, 500);
+		this._context = this.haCanvas.getContext('2d');
+		this.drawXCoordinates = [];
+	}
 
-    get context () {
-        return this._context
-    }
+	get context() {
+		return this._context;
+	}
 
-    get ctx () {
-        return this._ctx
-    }
+	get ctx() {
+		return this._ctx;
+	}
 
-    get wordToGuess () {
-        return this._wordToGuess
-    }
+	get wordToGuess() {
+		return this._wordToGuess;
+	}
 
-    set wordToGuess (word) {
-        this._wordToGuess = word
-    }
+	set wordToGuess(word) {
+		this._wordToGuess = word;
+	}
 
-    clearContext(){
-        this.ctx.clearRect(0,0,this.unCanvas.width, this.unCanvas.height)
-        this.context.clearRect(0,0,this.haCanvas.width,this.haCanvas.height)
-    }
+	clearContext() {
+		this.ctx.clearRect(0, 0, this.unCanvas.width, this.unCanvas.height);
+		this.context.clearRect(0, 0, this.haCanvas.width, this.haCanvas.height);
+	}
 
+	createCanvas(query, width, height) {
+		const canvas = document.querySelector(query);
+		canvas.width = width;
+		canvas.height = height;
+		return canvas;
+	}
 
-    createCanvas(query, width, height){
-        /** @type {HTMLCanvasElement} */
-        const canvas = document.querySelector(query)
-        canvas.width = width
-        canvas.height = height
-        return canvas
-    }
+	drawUnderscores() {
+		const lineLength = 45;
+		let beggining = 0;
+		for (let i = 0; i < this.wordToGuess.length; i++) {
+			let space = Math.floor(Math.random() * 10 + 20);
+			beggining = this.drawAUnderscore(beggining, lineLength, space);
+		}
+		this.moveToMiddle(beggining);
+	}
 
-    drawUnderscores(){
-        /** @type {HTMLCanvasElement} */
-        const lineLength = 45;
-        let beggining = 0;
-        for(let i = 0; i < this.wordToGuess.length; i++){
-            let space = Math.floor(Math.random()*10 + 20)
-            beggining = this.drawAUnderscore(beggining, lineLength, space)
-        } 
-        this.moveToMiddle(beggining)
-    }
+	moveToMiddle(length) {
+		let move = 850 - length;
+		this.unCanvas.style.paddingLeft = `${move}px`;
+	}
 
-    moveToMiddle(length){
-        let move = 850 - length
-        this.unCanvas.style.paddingLeft = `${move}px`
-    }
+	drawAUnderscore(beggining, lineLength, space) {
+		this.drawXCoordinates.push(beggining);
+		this.ctx.lineWidth = Math.floor(Math.random() * 2 + 4);
+		let heightA = Math.floor(Math.random() * 6) + 100;
+		let heightB = Math.floor(Math.random() * 6) + 100;
+		this.ctx.strokeStyle = '#e0dbd1';
+		this.ctx.beginPath();
+		this.ctx.moveTo(beggining, heightA);
+		beggining += lineLength;
+		this.ctx.lineTo(beggining, heightB);
+		this.ctx.stroke();
+		this.ctx.closePath;
+		beggining += space;
+		return beggining;
+	}
 
-    drawAUnderscore(beggining, lineLength, space){
-        /** @type {HTMLCanvasElement} */
-        this.drawXCoordinates.push(beggining)
-        this.ctx.lineWidth = Math.floor(Math.random()*2 + 4)
-        let heightA = Math.floor(Math.random()*6) + 100
-        let heightB = Math.floor(Math.random()*6) + 100
-        this.ctx.strokeStyle = '#e0dbd1'
-        this.ctx.beginPath();
-        this.ctx.moveTo(beggining, heightA)
-        beggining += lineLength
-        this.ctx.lineTo(beggining, heightB)
-        this.ctx.stroke()
-        this.ctx.closePath;
-        beggining += space
-        return beggining
-    }
+	drawLetter(letter, indices) {
+		this.ctx.font = '30px Arial';
+		this.ctx.fillStyle = '#e0dbd1';
+		for (let i = 0; i < indices.length; i++) {
+			this.ctx.font = '50px Comic Sans MS';
+			this.ctx.fillStyle = '#e0dbd1';
+			this.ctx.fillText(letter, this.drawXCoordinates[indices[i]] + 5, 90);
+		}
+	}
 
-    drawLetter (letter, indices) {
-        this.ctx.font = "30px Arial";
-        this.ctx.fillStyle = '#e0dbd1'
-        for(let i = 0; i < indices.length; i++){
-            this.ctx.font = "50px Comic Sans MS"
-            this.ctx.fillStyle = '#e0dbd1'
-            this.ctx.fillText(letter, this.drawXCoordinates[indices[i]] + 5,90)
-        }
-    }
+	drawHangman(badGuesses) {
+		this.context.strokeStyle = '#e0dbd1';
+		this.context.lineWidth = 3;
+		switch (badGuesses) {
+			//DRAW HEAD
+			case 2:
+				this.context.moveTo(300, 100);
+				this.context.beginPath();
+				this.context.arc(300, 150, 50, 0, 2 * Math.PI, true);
+				this.context.stroke();
+				break;
+			case 3:
+				//Draw corpse
+				this.drawALine(300, 200, 300, 350);
+				break;
+			case 4:
+				//Draw right arm
+				this.drawALine(300, 275, 375, 200);
+				break;
+			case 5:
+				//Draw left arm
+				this.drawALine(300, 275, 225, 200);
+				break;
+			case 6:
+				//Draw right leg
+				this.drawALine(300, 350, 350, 425);
+				break;
+			case 7:
+				//Draw left leg
+				this.drawALine(300, 350, 250, 425);
+				break;
+		}
+	}
 
-    drawHangman (badGuesses) {
-        this.context.strokeStyle = '#e0dbd1'
-        this.context.lineWidth = 3
-        switch (badGuesses) {
-            //DRAW HEAD
-            case 2:
-                this.context.moveTo(300, 100)
-                this.context.beginPath()
-                this.context.arc(300, 150, 50, 0, 2*Math.PI, true)
-                this.context.stroke();
-                break;
-            case 3:
-                //Draw corpse
-                this.drawALine(300, 200, 300, 350)
-                break;
-            case 4:
-                //Draw right arm
-                this.drawALine(300, 275, 375, 200)
-                break;
-            case 5:
-                //Draw left arm
-                this.drawALine(300, 275, 225, 200)
-                break;
-            case 6:
-                //Draw right leg
-                this.drawALine(300, 350, 350, 425)
-                break;
-            case 7:
-                //Draw left leg
-                this.drawALine(300, 350, 250, 425)
-                break;
-        }
-    }
+	drawALine(x1, y1, x2, y2) {
+		this.context.moveTo(x1, y1);
+		this.context.lineTo(x2, y2);
+		this.context.stroke();
+	}
 
-    drawALine (x1, y1, x2, y2) {
-        this.context.moveTo(x1, y1)
-        this.context.lineTo(x2, y2)
-        this.context.stroke()
-    }
-
-    drawHanger () {
-        /** @type {HTMLCanvasElement} */
-        this.context.strokeStyle = '#e0dbd1'
-        this.context.lineWidth = 5
-        this.context.beginPath();
-        this.drawALine(50, 490, 150, 490)
-        this.drawALine(100, 490, 100, 50)
-        this.context.lineTo(300, 50)
-        this.context.lineTo(300, 100)
-        this.context.stroke()
-        this.context.closePath();
-}
+	drawHanger() {
+		this.context.strokeStyle = '#e0dbd1';
+		this.context.lineWidth = 5;
+		this.context.beginPath();
+		this.drawALine(50, 490, 150, 490);
+		this.drawALine(100, 490, 100, 50);
+		this.context.lineTo(300, 50);
+		this.context.lineTo(300, 100);
+		this.context.stroke();
+		this.context.closePath();
+	}
 }
 
 export default Draw;
